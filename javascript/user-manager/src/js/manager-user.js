9ca7fb569ea1a58a062feb69384eec38792afe0b
Tam Tran <tam.tran@asnet.com.vn>
2015-06-04 18:14:51 +0700

    Add class add new user into DOM

--- javascript/user-manager/src/js/manager-user.js
+++ javascript/user-manager/src/js/manager-user.js
@@ -16,6 +16,21 @@ var Application = Application || {};
 		});
 	};
 
+		// add a new user
+	UserManager.prototype.addUser = function addUser(listUsers, currentId) {
+	 	// body...
+	 	var id = this.store.getCurrentId();
+	 	var name = $('#userName').val();
+	 	var address = $('#userAddress').val();
+	 	var email = $('#userEmail').val();
+
+	 	var user = new App.UserStore(id++, name, address, email);
+
+	 	this.listUsers.push(user);
+	 	users.viewUser();
+	 	this.store.saveUser(this.listUsers, id);
+	 };
+
 	App.UserManager = UserManager;
 
 })(Application);
