146e9bca5f0328de278323afa241095085f5d254
Tam Tran <tam.tran@asnet.com.vn>
2015-06-05 14:16:54 +0700

    add method delete user

--- javascript/user-manager/src/js/manager-user.js
+++ javascript/user-manager/src/js/manager-user.js
@@ -11,25 +11,37 @@ var Application = Application || {};
 
 	// view all users to id #view-all-user
 	UserManager.prototype.viewAllUser = function viewAllUser(thisObj) {
-		_.forEach(thisObj.listUsers, function(users) {
-			users.viewUser();
+		_.forEach(thisObj.listUsers, function(user) {
+			user.viewUser();
 		});
 	};
 
-		// add a new user
-	UserManager.prototype.addUser = function addUser(listUsers, currentId) {
+	// add a new user
+	UserManager.prototype.addUser = function addUser() {
 	 	// body...
 	 	var id = this.store.getCurrentId();
 	 	var name = $('#userName').val();
 	 	var address = $('#userAddress').val();
 	 	var email = $('#userEmail').val();
 
-	 	var user = new App.UserStore(id++, name, address, email);
+	 	var user = new App.User(id++, name, address, email);
 
 	 	this.listUsers.push(user);
-	 	users.viewUser();
+	 	user.viewUser();
 	 	this.store.saveUser(this.listUsers, id);
 	 };
+
+	 // remove a user
+	 UserManager.prototype.delUser = function delUser(userNode, nodeId) {
+	 	// body...
+ 	 	userNode.remove();
+		_.remove(this.listUsers, function(user) {
+			return parseInt(user.getId()) === parseInt(nodeId);
+		});
+
+		// save change after delete user
+	 	this.store.saveUser(this.listUsers);
+	};
 
 	App.UserManager = UserManager;
 
