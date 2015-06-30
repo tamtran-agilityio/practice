bed3bbfc0dc320c14b575244ccdd2b2c515e0289
Tam Tran <tam.tran@asnet.com.vn>
2015-06-30 19:05:10 +0700

    Script parallax part header and driver

--- javascript/sparks-scroll/src/js/main.js
+++ javascript/sparks-scroll/src/js/main.js
@@ -1,3 +1,72 @@
-var s = skrollr.init();
+// var s = skrollr.init();
 
-skrollr.init({forceHeight: false});
+// skrollr.init({forceHeight: false});
+
+var parallaxContainer = $('.background-basket').height();
+
+$(window).scroll(function() { 
+	// body...
+	var sparkScroll = $(this).scrollTop();
+
+	//
+	if (sparkScroll <= parallaxContainer) {
+
+		$('.basket-line-long').css({
+			'transform' : 'translate(0px, '+ sparkScroll /60 +'%)'
+		});
+
+		$('.basket-iphone').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /50 +'%)'
+		});
+
+		$('.background-basket').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /60 +'%)'
+		});
+
+		$('.txt-wapper').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /20 +'%)'
+		});
+
+		$('.basket-line-long').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /15 +'%)'
+		});
+
+		$('.basket-radio').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
+		});
+
+		$('.basket-line-short').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
+		});
+
+		$('.basket-globes').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
+		});
+
+		$('.basket-line-short-last').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
+		});
+
+		$('.basket-cloud').css({
+			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
+		});
+	}
+	
+	if(sparkScroll > $('.wapper-content').offset().top - $(window).height()){
+
+		$('.wapper-content').css({'background-position':'center '+ (sparkScroll - $('.wapper-content').offset().top) +'px'});
+
+		var opacity = (sparkScroll - $('.wapper-content').offset().top + 200) / (sparkScroll / 5);
+
+		$('.sparks-what-text').css({'opacity': opacity});
+
+		$('.fan-iphone').css({'opacity': opacity});
+
+		$('.app-iphone').css({'opacity': opacity});
+
+		$('.channels-text').css({'opacity': opacity});
+
+		$('.background-sparks').css({'opacity': opacity});
+
+	}
+});
