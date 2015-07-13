5ac8ba4662e93a3aac1427ff1c3ad9d3f644b761
Tam Tran <tam.tran@asnet.com.vn>
2015-07-13 11:22:59 +0700

    Styles script parallax of part digital drive

--- javascript/sparks-scroll/src/js/main.js
+++ javascript/sparks-scroll/src/js/main.js
@@ -1,209 +1,97 @@
+// /* Script of parallax scrolling*/
+// $(window).scroll(function() { 
+// 	// body...
+// 	var sparkScroll = $(this).scrollTop();
+// 	// Check value of image background basket
+// 	var parallaxContainer = $('.bg-pitch-basket').height();
+// 	var flag = false;
+// 	// Set transform of part logo top
+// 	if ((sparkScroll <= parallaxContainer) && (flag === false)) {
 
+// 		var opacity = (sparkScroll - $('.bg-pitch-basket').offset().top + 200) / (sparkScroll /4);
+// 		$('.bg-pitch-basket').css({'background-position':'center '+ (sparkScroll - $('.bg-pitch-basket').offset().top) +'px'});
+// 		flag = true;
+// 	}
 
-$(window).scroll(function() { 
-	// body...
-	var sparkScroll = $(this).scrollTop();
+// 	// Set property opacity of part what sparks
+// 	if((sparkScroll > $('.sparks-insiders').offset().top - $(window).height())  && (flag !== true))  {
 
-	// Check value of image background basket
-	var parallaxContainer = $('.background-basket').height();
+// 		// Set property background position content what spark
+// 		$('.sparks-insiders').css({'background-position':'center '+ (sparkScroll - $('.sparks-insiders').offset().top) +'px'});
+// 		// Set property background opacity content what spark
+// 		$('.contain-ball').css({'opacity': opacity});
+// 	}
 
-	// Set transform of part logo top
-	if (sparkScroll <= parallaxContainer) {
+// 	// Set property opacity of part how how-does
+// 	if((sparkScroll > $('.influence-digital').offset().top - $(window).height()) && (flag === false)) {
 
-		var opacity = (sparkScroll - $('.background-basket').offset().top + 200) / (sparkScroll / 100);
-
-		// Set property transform of traditional sparks
-		$('.basket-line-long').css({
-			'transform' : 'translate(0px, '- sparkScroll /60 +'%)'
-		});
-
-		$('.basket-iphone').css({
-			'transform' : 'translate(0px, -'- sparkScroll /50 +'%)'
-		});
-
-		$('.background-basket').css({
-			'transform' : 'translate(0px, -'- sparkScroll /60 +'%)'
-		});
-
-		$('.exchange-info').css({
-			'transform' : 'translate(0px, -'- sparkScroll /200 +'%)'
-		});
-
-		$('line-long').css({
-			'transform' : 'translate(0px, -'+ sparkScroll /15 +'%)'
-		});
-
-		$('.radio-airwaves').css({
-			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
-		});
-
-		$('.line-short').css({
-			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
-		});
-
-		$('.globes-online').css({
-			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
-		});
-
-		$('.line-short-last').css({
-			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
-		});
-
-		$('.sparks-cloud').css({
-			'transform' : 'translate(0px, -'+ sparkScroll /10 +'%)'
-		});
-	}
-	
-	// Set property opacity of part what sparks
-	if(sparkScroll > $('.content-sparks').offset().top - $(window).height()) {
-
-		var opacity = (sparkScroll - $('.content-sparks').offset().top + 200) / (sparkScroll / 100);
-		// Set property background position content what spark
-		$('.what-sparks').css({'background-position':'center '+ (sparkScroll - $('.what-sparks').offset().top) +'px'});
-		$('.background-basket').css({'background-position':'center '+ (sparkScroll - $('.background-basket').offset().top) +'px'});
-		// Set property background opacity content what spark
-		$('.sparks-what-text').css({'opacity': opacity});
-		$('.fan-iphone').css({'opacity': opacity});
-		$('.app-iphone').css({'opacity': opacity});
-		$('.channels-text').css({'opacity': opacity});
-		$('.background-sparks').css({'opacity': opacity});
-
-	}
-
-	// Set property opacity of part how does
-	if(sparkScroll > $('.benefit-content').offset().top - $(window).height()) {
-
-		var opacitysText = (sparkScroll - $('.benefit-content').offset().top + 500) / (sparkScroll / 100);
-		var opacityVisual = (sparkScroll - $('.comment-visual').offset().top + 800) / (sparkScroll / 100);
-		var opacityArrow = (sparkScroll - $('.comment-visual').offset().top + 700) / (sparkScroll / 50);
-		var opacityTweet = (sparkScroll - $('.comment-visual').offset().top + 600) / (sparkScroll / 50);
-		var opacityArrowRight = (sparkScroll - $('.comment-visual').offset().top + 700) / (sparkScroll / 100);
-		var opacityTweetUser = (sparkScroll - $('.comment-visual').offset().top + 500) / (sparkScroll / 80);
-		var opacityTopic = (sparkScroll - $('.comment-visual').offset().top + 300) / (sparkScroll / 100);
+// 		// var opacitysVisual = (sparkScroll - $('.influence-digital__visual').offset().top + 800) / (sparkScroll / 2);
+// 		var opacityDriver = (sparkScroll - $('.influence-digital__drive').offset().top + 1500) / (sparkScroll / 2);
+// 		var opacityBenefit = (sparkScroll - $('.influence-digital__benefit').offset().top + 800) / (sparkScroll / 2);
+// 		var opacityWall = (sparkScroll - $('.bg-tweet__wall').offset().top + 800) / (sparkScroll / 8);
+// 		var opacityArrowLeft = (sparkScroll - $('.bg-arrow__left').offset().top + 1000) / (sparkScroll / 8);
+// 		var opacityArrowRight = (sparkScroll - $('.bg-arrow__right').offset().top + 1000) / (sparkScroll / 6);
 		
-		$('.how-does').css({'background-position':'center '+ (sparkScroll - $('.how-does').offset().top) +'px'});
-		// Set background position content benefit
-		$('.benefit-content').css({'background-position':'center '+ (sparkScroll - $('.benefit-content').offset().top) +'px'});
-		
-		// Set property opacity content benefit
-		$('.benefit-content').css({'opacity': opacitysText});
+// 		// Set property opacity grow content
+// 		$('.influence-digital__drive').css({'opacity': opacityDriver});
 
-		// Set property opacity grow content
-		$('.grow-content').css({'opacity': opacitysText});
+// 		// Set property opacity comment visual	
+// 		$('.influence-digital__benefit').css({'opacity': opacityBenefit});
 
-		// Set property opacity comment visual	
-		$('.comment-visual').css({'opacity': opacityVisual});
+// 		// Set property opacity Tweet wall
+// 		$('.bg-tweet__wall').fadeIn(3000);
+// 		// $('.bg-tweet__wall').css({'opacity': opacityWall});
 
-		// Set property opacity image arrow left
-		$('.arrow-left').css({'opacity': opacityArrow});	
+// 		// Set property opacity Tweet wall	
+// 		$('.bg-arrow__left').css({'opacity': opacityArrowLeft});
 
-		// Set property opacity image tweet wall
-		$('.tweet-wall').css({'opacity': opacityTweet});
-
-		// Set property opacity image comtent comment
-		$('.content-comment').css({'opacity': opacityTweet});
-
-		// Set property opacity image comment list
-		$('.comment-list').css({'opacity': opacityTweet});
-
-		// Set property opacity image arrow right
-		$('.arrow-right').css({'opacity': opacityArrowRight});
-
-		// Set property opacity image comment user
-		$('.content-user').css({'opacity': opacityTweetUser});
-
-		// Set property opacity text topic content
-		$('.topic-content').css({'opacity': opacityTopic});
-	}
-
-	// Set background and opacity of ipad
-	if(sparkScroll > $('.topic-tweet').offset().top - $(window).height()) {
-
-		var opacityTopicTweet = (sparkScroll - $('.topic-tweet').offset().top + 800) / (sparkScroll / 100);
-		var opacityArrowHorizontal = (sparkScroll - $('.topic-tweet').offset().top +800) / (sparkScroll / 100);
-		var opacityIpad = (sparkScroll - $('.topic-tweet').offset().top +700) / (sparkScroll / 100);
-		
-		// Set property background position of slide ipad
-		$('.screenshot').css({'background-position':'center '+ (sparkScroll - $('.screenshot').offset().top) +'px'});
-		
-		// Set property opacity of image topic tweet
-		$('.topic-tweet').css({'opacity': opacityTopicTweet});
-
-		// Set property opacity of image arrow horizontal
-		$('.arrow-horizontal').css({'opacity': opacityArrowHorizontal});
-
-		// Set property opacity of image ipad
-		$('.background-ipad').css({'opacity': opacityIpad});
-	}
-
-	// Promoscope content ipad
-	if(sparkScroll > $('.background-ipad').offset().top - $(window).height()){
-
-
-		var opacityContent = (sparkScroll - $('.background-ipad').offset().top + 500) / (sparkScroll / 100);
-		
-		//Set property background of content ipad
-		$('.content-ipad').css({'background-position':'center '+ (sparkScroll - $('.content-ipad').offset().top) +'px'});
-		
-		//Set property opacity of image content ipad
-		$('.content-ipad').css({'opacity': opacityContent});
-	}
-});
-
-// Set top down of image 
-// $(document).ready(function() {
-		
-// 		function beeTop() {
-// 				$(".content-ipad").animate({top: "-=500"}, 1000, "swing", beeDown);
-// 		}
-// 		function beeDown() {
-// 				$(".content-ipad").animate({top: "+=500"}, 1000, "swing", beeTop);
-// 		}
-		
-// 		beeDown();
+// 		// Set property opacity Tweet wall	
+// 		$('.bg-arrow__right').css({'opacity': opacityArrowRight});
+// 	}
 // });
 
-//  Scrolling image content in image ipade
-$(function(){
-	var scroller = $('.background-ipad div.content-ipad');
-	var scrollerContent = scroller.children('ul');
-	scrollerContent.children().clone().appendTo(scrollerContent);
-	var curX = 0;
-	scrollerContent.children().each(function(){
-			var $this = $(this);
-			$this.css('top', curX);
-			curX += $this.outerWidth(true);
-	});
-	var fullW = curX * 2;
-	var viewportW = scroller.width();
+var action;
 
-	// Scrolling speed management
-	var controller = {curSpeed:0, fullSpeed:4};
-	var $controller = $(controller);
-	var tweenToNewSpeed = function(newSpeed, duration)
-	{
-			if (duration === undefined)
-					duration = 600;
-			$controller.stop(true).animate({curSpeed:newSpeed}, duration);
-	};
+$(window).scroll(function() {
 
-	// Pause on hover
-	scroller.hover(function(){
-			tweenToNewSpeed(0);
-	}, function(){
-			tweenToNewSpeed(controller.fullSpeed);
-	});
+	var sparkScroll = $(this).scrollTop();
+	var heightBasket = $('.bg-pitch-basket').height();
+	var heightSilver = $('.screenshot-silver').height();
 
-	// Scrolling management; start the automatical scrolling
-	var doScroll = function()
-	{
-			var curX = scroller.scrollTop();
-			var newX = curX + controller.curSpeed;
-			if (newX > fullW*0.5 - viewportW + 200)
-					newX -= fullW;
-			scroller.scrollTop(newX);
-	};
+	if (sparkScroll <= heightBasket) {
+		$('.bg-pitch-basket').css({'background-position':'center '+ (sparkScroll - $('.bg-pitch-basket').offset().top) +'px'});
+	}
 
-	setInterval(doScroll, 100);
-	tweenToNewSpeed(controller.fullSpeed);
+	if (sparkScroll <= heightSilver) {
+		$('.screenshot-silver').css({'background-position':'center '+ (sparkScroll - $('.screenshot-silver').offset().top) +'px'});
+	}
+
+	clearTimeout(action);
+	scrollParallax();
 });
+
+function scrollParallax() {
+
+	var heightInfluence = $('.influence-digital').height();
+
+	action = setTimeout(function() {
+		
+		// fade in block driver 
+		if ($(this).scrollTop() > (heightInfluence/6)) {
+			$('.influence-digital__drive').fadeIn();
+		}
+
+		if ($(this).scrollTop() > (heightInfluence/4)) {
+			$('.bg-arrow__left').fadeIn();
+		}
+
+		if ($(this).scrollTop() > (heightInfluence/4)) {
+			$('.bg-arrow__right').fadeIn();
+		}
+
+		if ($(this).scrollTop() > (heightInfluence)) {
+			$('.bg-tweet__wall').fadeIn();
+		}
+
+	}, 200);
+}
