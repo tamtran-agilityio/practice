d02a1783333d888998e91c543e5434f81f7ad141
Tam Tran <tam.tran@asnet.com.vn>
2015-07-15 18:13:24 +0700

    Fix parallax of page and fix js code styles

--- javascript/sparks-scroll/src/js/main.js
+++ javascript/sparks-scroll/src/js/main.js
@@ -1,64 +1,76 @@
-/* script of parallax scrolling*/
+/* global script of parallax scrolling*/
 
+/**
+ * scrollParallax execute fade in when scrolling into
+ */
 var action;
 
 function scrollParallax() {
+  var digitalDrive = $('.influence-digital__drive');
+  var topicTweet = $('.bg-tweet__wall');
+  var arrowLeft = $('.bg-arrow__left');
+  var arrowRight = $('.bg-arrow__right');
+  var arrowHorizon = $('.screenshot__arrow--horizontal');
+  var expandedDetails = $('.screenshot__expanded--details');
+  var expandedPut = $('.screenshot__expanded--put');
+  var heightInfluence = $('.influence-digital').height();
+  var heightScreenshot = $('.screenshot-silver').height();
 
-	var heightInfluence = $('.influence-digital').height();
-	var heightScreenshot = $('.screenshot-silver').height();
-	var heightHorizontal = $('.screenshot__arrow--horizontal').height();
+  action = setTimeout(function() {
 
-	action = setTimeout(function() {
+  // fade in block influence-digital
+  if (($(this).scrollTop()) > (heightInfluence / 3)) {
+    digitalDrive.fadeIn();
+    topicTweet.fadeIn();
+  }
 
-	// fade in block influence-digital
-	if (($(this).scrollTop()) > (heightInfluence / 3)) {
-		$('.influence-digital__drive').fadeIn();
-		$('.bg-tweet__wall').fadeIn();
-	}
+  // fade in arrow left, right
+  if (($(this).scrollTop()) > (heightInfluence * 1.2)) {
+    arrowLeft.fadeIn();
+    arrowRight.fadeIn();
+  }
 
-	// fade in arrow left, right
-	if (($(this).scrollTop()) > (heightInfluence * 1.2 )) {
-		$('.bg-arrow__left').fadeIn();
-		$('.bg-arrow__right').fadeIn();
-	}
+  // fade in block screenshot silver
+  if ($(this).scrollTop() > (heightScreenshot)) {
+    expandedDetails.fadeIn();
+    expandedPut.fadeIn();
+  }
 
-	// fade in block screenshot silver
-	if ($(this).scrollTop() > (heightScreenshot)) {
-		$('.screenshot__expanded--details').fadeIn();
-		$('.screenshot__expanded--put').fadeIn();
-	}
+  if ($(this).scrollTop() > (heightScreenshot * 2.5)) {
+    arrowHorizon.fadeIn();
+  }
 
-	if ($(this).scrollTop() > (heightScreenshot * 2.5)) {
-		$('.screenshot__arrow--horizontal').fadeIn();
-	}
-
-	}, 200);
+  }, 300);
 }
 
+/**
+ * handle event when scrolling
+ */
 $(window).scroll(function() {
 
-	var sparkScroll = $(this).scrollTop();
-	var sparkScrollExpanded = $('.screenshot__expanded--in').offset().top;
-	var heightExpanded = $('.screenshot__expanded--in').height();
-	var heightBasket = $('.bg-pitch-basket').height();
-	var heightSilver = $('.screenshot-silver').height();
-	var heightBallLearn = $('.bg-ball__learn').height();
+  var sparkScroll = $(this).scrollTop();
+  var sparkScrollExpanded = $('.screenshot__expanded--in').offset().top;
+  var heightBasket = $('.bg-pitch-basket').height();
+  var heightSilver = $('.screenshot-silver').height();
+  var bgPitch = $('.bg-pitch-basket');
+  var screenSilver = $('.screenshot-silver');
+  var expandedIn = $('.screenshot__expanded--in');
 
-	//  set background position of pitch basket
-	if (sparkScroll <= heightBasket) {
-		$('.bg-pitch-basket').css({'background-position':'center ' + (sparkScroll - $('.bg-pitch-basket').offset().top) + 'px'});
-	}
+  // set background position of pitch basket
+  if (sparkScroll <= heightBasket) {
+    bgPitch.css({'background-position':'center ' + (sparkScroll - bgPitch.offset().top) + 'px'});
+  }
 
-	// set background position of screenshot silver
-	if (sparkScroll <= heightSilver) {
-		$('.screenshot-silver').css({'background-position':'center ' + (sparkScroll - $('.screenshot-silver').offset().top) + 'px'});
-	}
+  // set background position of screenshot silver
+  if (sparkScroll <= heightSilver) {
+    screenSilver.css({'background-position':'center ' + (sparkScroll - screenSilver.offset().top) + 'px'});
+  }
 
-	// promoscope content ipad
-	if (sparkScroll > (sparkScrollExpanded)) {
-		$('.screenshot__expanded--in').css({'background-position':'0% ' + (sparkScroll - 15 - $('.screenshot__expanded--in').offset().top) + 'px'});
-	}
+  // promoscope content ipad
+  if (sparkScroll > sparkScrollExpanded) {
+    expandedIn.css({'background-position':'0% ' + (sparkScroll - expandedIn.offset().top) + 'px'});
+  }
 
-	clearTimeout(action);
-	scrollParallax();
+  clearTimeout(action);
+  scrollParallax();
 });
