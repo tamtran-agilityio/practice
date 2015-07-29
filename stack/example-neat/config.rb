cc78ffce40694d66f2f23e92a99aac825c969c9a
Tam Tran <tam.tran@asnet.com.vn>
2015-07-29 15:13:08 +0700

    delete foder bourbon and neat

--- stack/example-neat/config.rb
+++ stack/example-neat/config.rb
@@ -2,19 +2,24 @@ preferred_syntax = :sass
 
 http_path = "/"
 
-images_dir = "src/files/images"
 
 fonts_dir = "src/files/fonts"
 
 # Config image path
-http_images_path = "/images"
 
 # Config font path
 http_fonts_path = "/fonts"
 
 # Config http generated images path (for sprite background)
 
+images_dir = "src/images"
+http_images_path = "/images"
 http_generated_images_path = "/images"
+generated_images_dir = "out/images"
+# images_dir = "src/images"
+# http_images_path = "/images"
+# http_generated_images_path = "/images"
+# generated_images_dir = "out/images"
 
 line_comments = false
 
