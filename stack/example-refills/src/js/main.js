2459a45b6350c071b4cdf7ebc9f010ff1d3acfc8
Tam Tran <tam.tran@asnet.com.vn>
2015-07-30 16:21:40 +0700

    Use example of bitter do example refills

---
+++ stack/example-refills/src/js/main.js
@@ -0,0 +1,23 @@
+/* file javscript */
+// $(function() {
+//   $('.header-top').click(function () {
+//     $('.header-top').slideToggle("slow");
+//   });
+//   $('.header-top').css({"display":"none","transition":"none","max-height":"inherit"});
+//   $('#toggleMenu').remove();
+// });
+
+$(document).ready(function() {
+  var menuToggle = $('#centered-navigation-mobile-menu').unbind();
+  $('#centered-navigation-menu').removeClass("show");
+  
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#centered-navigation-menu').slideToggle(function(){
+      if($('#centered-navigation-menu').is(':hidden')) {
+        $('#centered-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
+
