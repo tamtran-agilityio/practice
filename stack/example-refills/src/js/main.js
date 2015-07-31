6f50ecec662981166a0f4e4577850cb325627fe1
Tam Tran <tam.tran@asnet.com.vn>
2015-07-31 08:52:55 +0700

    add link all file execute scroll on page

--- stack/example-refills/src/js/main.js
+++ stack/example-refills/src/js/main.js
@@ -1,5 +1,8 @@
 /* file javscript */
 
+/**
+ * show menu when size screen small than 768px
+ */
 $(document).ready(function() {
   var menuToggle = $('#centered-navigation-mobile-menu').unbind();
   $('#centered-navigation-menu').removeClass("show");
@@ -14,3 +17,36 @@ $(document).ready(function() {
   });
 });
 
+
+/**
+ * jum to section of link 
+ */
+(function (jQuery) {
+  jQuery.mark = {
+    jump: function (options) {
+      var defaults = {
+        selector: 'a.scroll-on-page-link'
+      };
+      if (typeof options == 'string') {
+        defaults.selector = options;
+      }
+
+      options = jQuery.extend(defaults, options);
+      return jQuery(options.selector).click(function (e) {
+        var jumpobj = jQuery(this);
+        var target = jumpobj.attr('href');
+        var thespeed = 1000;
+        var offset = jQuery(target).offset().top;
+        jQuery('html,body').animate({
+          scrollTop: offset
+        }, thespeed, 'swing');
+        e.preventDefault();
+      });
+    }
+  };
+})(jQuery);
+
+
+jQuery(function(){  
+  jQuery.mark.jump();
+});
