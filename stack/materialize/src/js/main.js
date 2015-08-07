11c2ddd297fb36580b6183826502a67ddbb21438
Tam Tran <tam.tran@asnet.com.vn>
2015-08-07 17:14:00 +0700

    call function execute slide, modal, parallax

--- stack/materialize/src/js/main.js
+++ stack/materialize/src/js/main.js
@@ -1,1 +1,17 @@
 /* file javscript */
+
+// star silde
+$(document).ready(function(){
+  $('.slider').slider({full_width: true});
+});
+
+// modal show
+$(document).ready(function(){
+  $('.modal-trigger').leanModal();
+});
+
+// execute parallax
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+      
