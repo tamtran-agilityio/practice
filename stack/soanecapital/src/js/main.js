ecfaf07e1652f6954ead3062412fe8823bf80048
Tam Tran <tam.tran@asnet.com.vn>
2015-08-13 18:13:55 +0700

    execute change background slide

--- stack/soanecapital/src/js/main.js
+++ stack/soanecapital/src/js/main.js
@@ -1,4 +1,27 @@
-/* file javscript */
+/*
+ * file javscript modal site
+ * -------------------------
+ */
 
-// set navigation
+/* set navigation */
 $(".button-collapse").sideNav();     
+
+/* set slide up background use fsvs plugin */
+$(document).ready( function() {
+  var fsvs = $.fn.fsvs({
+    speed : 600,
+    bodyID : 'fsvs-body',
+    selector : '> .slide',
+    mouseSwipeDisance : 40,
+    afterSlide : function(){},
+    beforeSlide : function(){},
+    endSlide : function(){},
+    mouseWheelEvents : true,
+    mouseDragEvents : true,
+    touchEvents : true,
+    arrowKeyEvents : true,
+    pagination : true,
+    nthClasses : false,
+    detectHash : true
+  });
+});
