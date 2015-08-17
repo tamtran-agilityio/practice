e69e17e8867121eccaada9e34763918f295a5f68
Tam Tran <tam.tran@asnet.com.vn>
2015-08-17 14:37:54 +0700

    Fix hidden text on slide

--- stack/soanecapital/src/js/fsvs.js
+++ stack/soanecapital/src/js/fsvs.js
@@ -23,6 +23,8 @@
       autoPlay : false,
       bodyID : 'fsvs-body',
       selector : '> .slide',
+      selectorTille : '.big-tille',
+      selectorDetais : '.details-content',
       mouseSwipeDisance : 40,
       afterSlide : function(){},
       beforeSlide : function(){},
@@ -551,7 +553,11 @@
         _body.removeClass( removeClass = 'active-slide-' + (before+1) );
         _body.addClass( 'active-slide-' + (after+1) );
         $( options.selector, body ).eq( before ).removeClass( 'active-slide' );
+        $( options.selectorTille, body ).eq( before ).removeClass('show-text');
+        $( options.selectorDetais, body ).eq( before ).removeClass('show-text');
         $( options.selector, body ).eq( after ).addClass( 'active-slide' );
+        $( options.selectorTille, body ).eq( after ).addClass( 'show-text' );
+        $( options.selectorDetais, body ).eq( after ).addClass( 'show-text' );
         if( options.nthClasses ) {
           _body.removeClass( 'active-nth-slide-' + (( before % options.nthClasses )+1) );
           _body.addClass( 'active-nth-slide-' + (( after % options.nthClasses )+1) );
--- stack/soanecapital/src/js/main.js
+++ stack/soanecapital/src/js/main.js
@@ -4,10 +4,11 @@
  */
 
 /* set navigation */
-$(".button-collapse").sideNav();     
+$(".button-collapse").sideNav(); 
+
 
 /* set slide up background use fsvs plugin */
-$(document).ready( function() {
+$(function() {
   var fsvs = $.fn.fsvs({
     speed : 600,
     bodyID : 'fsvs-body',
@@ -23,5 +24,38 @@ $(document).ready( function() {
     pagination : true,
     nthClasses : false,
     detectHash : true
+  });
+
+
+/////
+  // var $active = ($("#slide-1").hasClass("active-slide")) ;
+  // if ($active) {
+  //   $('.big-tille').addClass('show-text');
+  // }
+  // if (!$active) {
+  //   $('.big-tille').removeClass('show-text');
+  // }
+  ///  
+
+  function scrollParallax() {
+    var bigTop = $('#slide-1').offset().top;
+
+      // fade in arrow left, right
+    if (($(this).scrollTop()) > (bigTop )) {
+        ('.big-tille').fadeIn();
+    }
+  }
+});
+
+$(function() {
+  $(window).scroll(function() {
+    console.log('@--',$(this).scrollTop());
+    var bigTop =$('#slide-1').offset().top;
+    if ($(this).scrollTop() >= bigTop) {
+      alert('2');    
+    };
+    // setTimeout(function() {
+    //   scrollParallax();
+    // }, 200);
   });
 });
