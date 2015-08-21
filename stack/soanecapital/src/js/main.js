651a2dc51892d63a2252c7abe4ac774847ce0e0d
Tam Tran <tam.tran@asnet.com.vn>
2015-08-21 16:50:59 +0700

    create animation of page frequently question answer

--- stack/soanecapital/src/js/main.js
+++ stack/soanecapital/src/js/main.js
@@ -3,31 +3,35 @@
  * -------------------------
  */
 
-$(document).ready( function() {
+$(document).ready(function() {
   var fsvs = $.fn.fsvs({
-    speed : 600,
-    bodyID : 'fsvs-body',
-    selector : '> .slide',
-    mouseSwipeDisance : 40,
-    afterSlide : function(){},
-    beforeSlide : function(){},
-    endSlide : function(){},
-    mouseWheelEvents : true,
-    mouseDragEvents : true,
-    touchEvents : true,
-    arrowKeyEvents : true,
-    pagination : true,
-    nthClasses : false,
-    detectHash : true
+    speed: 600,
+    bodyID: 'fsvs-body',
+    selector: '> .slide',
+    mouseSwipeDisance: 40,
+    afterSlide: function() {},
+
+    beforeSlide: function() {},
+
+    endSlide: function() {},
+
+    mouseWheelEvents: true,
+    mouseDragEvents: true,
+    touchEvents: true,
+    arrowKeyEvents: true,
+    pagination: true,
+    nthClasses: false,
+    detectHash: true
   });
 
   /* set navigation */
-  $(".button-collapse").sideNav(); 
+  $('.button-collapse').sideNav();
 
-  /* show page index*/ 
+  /* show page index*/
   ;(function() {
     'use strict';
 
+    var scrolling = true;
     var $loader = $('.loader');
     var $main = $('.main-full');
     var $bigTitle = $('.big-tille');
@@ -36,37 +40,35 @@ $(document).ready( function() {
     var time = 1000;
     var $heightBoxes = $('.boxes-container');
     var $backFooter = $('.bachground-footer');
-    var $inforDirect = $(".infor__direct");
-    var $inforClose = $(".infor__close");
-    var $volumeUp = $(".sound__volume--up");
-    var $volumeOff = $(".sound__volume--off");
-    var $playMusic = $(".sound__text--state");
-    var $offMusic = $(".sound__text--separator");
-    var $aTop = $heightBoxes.height() + 117.188 ;
-    var $hiddenFooter = $heightBoxes.height() - 50;
-    var $head = $(this).scrollTop();
+    var $inforDirect = $('.infor__direct');
+    var $inforClose = $('.infor__close');
+    var $volumeUp = $('.sound__volume--up');
+    var $volumeOff = $('.sound__volume--off');
+    var $playMusic = $('.sound__text--state');
+    var $offMusic = $('.sound__text--separator');
+    var $boxQuestion = $('.question__box');
 
-    /* Show element */
+    /* show element */
     function showElement(element) {
       element.show();
     }
 
-    /* Hidden element */
+    /* hidden element */
     function hideElement(element) {
       element.hide();
     }
 
-    /* Add Class for element */
-    function addClass(element,nameclass) {
+    /* add Class for element */
+    function addClass(element, nameclass) {
       element.addClass(nameclass);
     }
 
-    /* Remove Class of element */
-    function removeClass(element,nameclass) {
+    /* remove Class of element */
+    function removeClass(element, nameclass) {
       element.removeClass(nameclass);
     }
 
-    /* Set timeout for the element execute element*/
+    /* set timeout for the element execute element*/
     function afterTime(section, callback, element, attribute) {
 
       setTimeout(function() {
@@ -77,100 +79,128 @@ $(document).ready( function() {
     /* function add atriblute trainstion css */
     function addAtrCss(element) {
       return {
-        "-webkit-transition": "all " + element + "ms ease",
-        "-moz-transition": "all " + element + "ms ease",
-        "-o-transition": "all " + element + "ms ease",
-        transition: "all " + element + "ms ease"
+        '-webkit-transition': 'all ' + element + 'ms ease',
+        '-moz-transition': 'all ' + element + 'ms ease',
+        '-o-transition': 'all ' + element + 'ms ease',
+        transition: 'all ' + element + 'ms ease'
       }
     }
 
-    // Animation for menu when click on it
+    // animation for menu when click on it
     function menuload() {
       afterTime(2000, showElement, $main);
-      afterTime(2000, showElement, $bigTitle);
       afterTime(2000, showElement, $downArrow);
-      afterTime(2000, showElement, $detailsContent);
     }
 
-    // When load page will show loader
+    // when load page will show loader
     $(window).on('load', function() {
-      afterTime(1500, hideElement, $loader);
+      afterTime(1200, hideElement, $loader);
       menuload();
     });
 
-    /* event handle click icon volume*/ 
-    $volumeUp.click( function() {
+    /* event handle click icon volume*/
+    $volumeUp.click(function() {
       $volumeOff.fadeIn();
       $volumeUp.fadeOut();
       $playMusic.fadeIn();
       $offMusic.fadeOut();
     })
 
-    $volumeOff.click( function() {
+    $volumeOff.click(function() {
       $volumeUp.fadeIn();
       $volumeOff.fadeOut();
       $playMusic.fadeOut();
       $offMusic.fadeIn();
     })
 
-    $offMusic.click( function() {
+    $offMusic.click(function() {
       $volumeOff.fadeIn();
       $volumeUp.fadeOut();
       $playMusic.fadeIn();
       $offMusic.fadeOut();
     })
 
-    $playMusic.click( function() {
+    $playMusic.click(function() {
       $volumeUp.fadeIn();
       $volumeOff.fadeOut();
       $playMusic.fadeOut();
       $offMusic.fadeIn();
     })
 
-    /* execute when scrolling */
-    $(window).scroll(function(){
+    /* set animation of page question and answer*/
+    var $window = $(window);
 
-      /* checking when to show footer*/  
-      if($head >= ($aTop)){
+    function viewBox() {
+      var windowTop = $window.scrollTop();
+      var windowHeight = $window.height();
+      var windowBottom = (windowTop + windowHeight);
+
+      $.each($boxQuestion, function() {
+        var $element = $(this);
+        var elementHeight = $element.outerHeight();
+        var windowTop = $element.offset().top;
+        var elementBottom = (windowTop + elementHeight);
+
+        // check to see if this current container is within viewport
+        if ((elementBottom >= windowTop) &&
+          (windowTop <= windowBottom)) {
+          $element.addClass('question__active');
+        } else {
+          $element.removeClass('question__active');
+        }
+      });
+    }
+
+    /* show and hidden footer*/
+    function showFooter() {
+      var $aTop = $heightBoxes.height() + 117.188;
+      var $hiddenFooter = $heightBoxes.height() - 80;
+      var $head = $(this).scrollTop();
+
+      /* checking when to show footer*/
+      if ($head >= ($aTop)) {
         $backFooter.fadeIn();
       }
 
       /* checking when to hidden footer*/
-      if($head < ($hiddenFooter)){
+      if ($head < ($hiddenFooter)) {
         $backFooter.fadeOut();
       }
 
       /* set event when click button direction*/
-      $inforDirect.click( function() {
-          $backFooter.css({
-            bottom: '0'
-          });
-          $inforClose.css({
-            opacity: '1'
+      $inforDirect.click(function() {
+        $backFooter.css({
+          bottom: '0'
+        });
+        $inforClose.css({
+          opacity: '1'
         });
       });
 
       /* set event when click button direction close*/
-      $inforClose.click( function()
-        {
-          $backFooter.css({
-            bottom: '-350px'
-          });
-          $inforClose.css({
-            opacity: '0'
-          });
-        }
-      );
-    });
+      $inforClose.click(function() {
+        $backFooter.css({
+          bottom: '-350px'
+        });
+        $inforClose.css({
+          opacity: '0'
+        });
+      });
+    }
+
+    $window.on('scroll resize', viewBox);
+    $window.on('scroll resize', showFooter);
+    $window.trigger('scroll');
   })();
 });
 
 function initialize() {
   var mapProp = {
-    center: new google.maps.LatLng(51.508742,-0.120850),
+    center: new google.maps.LatLng(51.508742, -0.120850),
     zoom:9,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map=new google.maps.Map(document.getElementById("map"), mapProp);
+  var map = new google.maps.Map(document.getElementById('map'), mapProp);
 }
-//google.maps.event.addDomListener(window, 'load', initialize);
+
+// google.maps.event.addDomListener(window, 'load', initialize);
