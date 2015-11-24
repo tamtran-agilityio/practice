d300ce0172b2805575d57d6050f7f745520badd3
Tam Tran <tam.tran@asnet.com.vn>
2015-11-24 08:20:53 +0700

    Modify function slider

--- Foodsense/Foodsense-Gulp-Yeoman/app/scripts/main.js
+++ Foodsense/Foodsense-Gulp-Yeoman/app/scripts/main.js
@@ -1,181 +1,53 @@
-/* file javscript */
-'use strict';
+/* Javascript file */
+!(function ( $ ) {
+  'use strict';
 
-var Application = {};
+  var triggers = $('ul.navigation li');
+  var images = $('.slide__item');
+  var lastElem = triggers.length-1;
+  var target;
 
-(function($, W, A) {
+  triggers.first().addClass('active');
+  images.hide().first().show();
 
-  A.App = function() {
+  function sliderResponse(target) {
+    images.fadeOut(300).eq(target).fadeIn(300);
+    triggers.removeClass('active').eq(target).addClass('active');
+  }
 
-    var $slide_Active = $('.slide__item');
-    var $feature = $('.navigation__item');
-    var $active = 'active';
-    var $page_Active = $('body');
-
-    // Remove all class active at slide image and navigation of Slide
-    var remove_Class = function() {
-      $feature.removeClass($active);
-      $slide_Active.removeClass($active);
-    };
-
-    // Click on navigation of slide image
-    var change_Slide = function() {
-
-      $feature.on('click', function(event) {
-
-        event.preventDefault();
-        var hrefTarget = $(this).children('a').attr('href');
-        remove_Class();
-        $(hrefTarget).addClass($active);
-        $(this).addClass($active);
-      });
-    };
-
-    // set handle image slides
-    var slideImage = function() {
-      $feature.each(function(i) {
-        if($($feature[i]).hasClass($active)) {
-          if($($feature[i]).is(':nth-child(4)')) {
-            remove_Class();
-            $($slide_Active[0]).addClass($active);
-            $($feature[0]).addClass($active);
-            return false;
-
-          } else {
-
-            remove_Class();
-            $($slide_Active[i + 1]).addClass($active);
-            $($feature[i + 1]).addClass($active);
-            return false;
-          }
-        }
-      });
-    };
-
-    // check title page active
-    function set_Page_Active(element, node) {
-      if ($('body').hasClass(element)) {
-        $('.item').find(node).addClass('active');
-      } else {
-        $('.item').find(node).removeClass('active');
-      }
+  triggers.click(function() {
+    if ( !$(this).hasClass('active') ) {
+      target = $(this).index();
+      sliderResponse(target);
+      resetTiming();
     }
+  });
 
-    this.init = function() {
+  function sliderTiming() {
+    target = $('ul.navigation li.active').index();
+    target === lastElem ? target = 0 : target = target+1;
+    sliderResponse(target);
+  }
 
-      // execute function use lazyload
-      $(W).bind('load', function() {
+  var timingRun = setInterval(function() { sliderTiming(); },5000);
+  function resetTiming() {
+    clearInterval(timingRun);
+    timingRun = setInterval(function() { sliderTiming(); },5000);
+  }
 
-        $('img').lazyload({
-          effect: 'fadeIn'
-        });
-      });
+  // check title page active
+  function set_Page_Active(element, node) {
+    if ($('body').hasClass(element)) {
+      $('.item').find(node).addClass('active');
+    } else {
+      $('.item').find(node).removeClass('active');
+    }
+  }
+  // get tag select
+  set_Page_Active('tastemaker', '.item__tastemakers');
+  set_Page_Active('blog', '.item__blogs');
+  set_Page_Active('a-list', '.item__vendors');
+  set_Page_Active('contribute', '.item__contribute');
+  set_Page_Active('about', '.item__about');
 
-      // set time for each slide
-      setInterval(function() {
-        slideImage();
-      }, 3500);
-
-      // get tag select
-      set_Page_Active('tastemaker', '.item__tastemakers');
-      set_Page_Active('blog', '.item__blogs');
-      set_Page_Active('a-list', '.item__vendors');
-      set_Page_Active('contribute', '.item__contribute');
-      set_Page_Active('about', '.item__about');
-
-      // call function change slide
-      change_Slide();
-    };
-  };
-
-}(window.jQuery, window, Application));
-
-window.jQuery(document).ready(function() {
-  new Application.App().init();
-});
-
-// !(function ( $ ) {
-//   'use strict';
-
-//   var $slide_Active = $('.slide__item');
-//   var $feature = $('.navigation__item');
-//   var $active = 'active';
-//   var $page_Active = $('body');
-
-//   // Click on navigation of slide image
-//   var change_Slide = function() {
-
-//     $feature.on('click', function(event) {
-
-//       event.preventDefault();
-//       var hrefTarget = $(this).children('a').attr('href');
-//       $feature.removeClass($active);
-//       $slide_Active.removeClass($active);
-//       $(hrefTarget).addClass('active');
-//       $(this).addClass('active');
-//       $(hrefTarget).fadeIn();
-//       $(this).fadeIn();
-//     });
-//   };
-//   change_Slide();
-
-//   function change_Slider(item, element) {
-
-//     var change_img_time     = 3500;
-//     var transition_speed    = 100;
-
-//     var simple_slideshow    = $(item),
-//       listItems           = simple_slideshow.children(element),
-//       listLen             = listItems.length,
-//       i                   = 0,
-
-//       changeList = function () {
-
-//         listItems.eq(i).fadeOut(transition_speed, function () {
-//           i += 1;
-//           if (i === listLen) {
-//               i = 0;
-//           }
-//           listItems.eq(i).fadeIn(transition_speed);
-//         });
-//       };
-
-
-//     listItems.not(':first').hide();
-//     setInterval(changeList, change_img_time);
-//   };
-
-//   function change_navigation() {
-//     if ($('#slide-1').css('display') =='none') {
-//       $('.navigation__item').removeClass('active');
-//     } else {
-//       $('.navigation__item').addClass('active');
-//     }
-//   }
-//   // change_navigation();
-
-//  // check title page active
-//   function set_Page_Active(element, node) {
-//     if ($('body').hasClass(element)) {
-//       $('.item').find(node).addClass('active');
-//     } else {
-//       $('.item').find(node).removeClass('active');
-//     }
-//   }
-
-//   change_Slider('.slide', 'section');
-
-//   set_Page_Active('tastemaker', '.item__tastemakers');
-//   set_Page_Active('blog', '.item__blogs');
-//   set_Page_Active('a-list', '.item__vendors');
-//   set_Page_Active('contribute', '.item__contribute');
-//   set_Page_Active('about', '.item__about');
-
-//   $(window).bind('load', function() {
-
-//     $('img').lazyload({
-//       effect: 'fadeIn'
-//     });
-//   });
-
-// }( jQuery ));
+}( jQuery ));
