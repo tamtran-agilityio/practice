886bd22bc42930167e4d388b0a4d978eca23e3ef
Tam Tran <tam.tran@asnet.com.vn>
2015-11-24 15:49:03 +0700

    add command to file script

--- Foodsense/Foodsense-Gulp-Yeoman/app/scripts/main.js
+++ Foodsense/Foodsense-Gulp-Yeoman/app/scripts/main.js
@@ -2,20 +2,28 @@
 !(function ( $ ) {
   'use strict';
 
-  var triggers = $('ul.navigation li');
-  var images = $('.slide__item');
-  var lastElem = triggers.length - 1;
+  var $li = $('ul.navigation li');
+  var $listSlide = $('.slide__item');
+  var lastElem = $li.length - 1;
   var target;
 
-  triggers.first().addClass('active');
-  images.hide().first().show();
+  // get tag select
+  setPageActive('tastemaker', '.item__tastemakers');
+  setPageActive('blog', '.item__blogs');
+  setPageActive('a-list', '.item__vendors');
+  setPageActive('contribute', '.item__contribute');
+  setPageActive('about', '.item__about');
 
-  function sliderResponse(target) {
-    images.fadeOut(300).eq(target).fadeIn(300);
-    triggers.removeClass('active').eq(target).addClass('active');
-  }
+  // Use plugin lazyload load image
+  $(window).bind('load', function() {
+    $('img').lazyload({
+      effect: 'fadeIn'
+    });
+  });
 
-  triggers.click(function() {
+  // Handle change slide when click navigation
+  $li.click(function() {
+    // check condition item current
     if ( !$(this).hasClass('active') ) {
       target = $(this).index();
       sliderResponse(target);
@@ -24,37 +32,45 @@
     }
   });
 
+  // add class actice navigation first child
+  $('ul.navigation li:first-child').addClass('active');
+  // add class actice slider first child
+  $listSlide.hide().first().show();
+
+  /** function to helper **/
+
+  // function to handle show slider
+  function sliderResponse(target) {
+    $listSlide.fadeOut(300).eq(target).fadeIn(300);
+    $li.removeClass('active').eq(target).addClass('active');
+  }
+
+  // function to show slide next
   function sliderTiming() {
+    // set value item current
     target = $('ul.navigation li.active').index();
+
+    // check condition to determine slide next
     target === lastElem ? target = 0 : target = target + 1;
     sliderResponse(target);
   }
 
+  // get vaeiable time call function show slide
   var timingRun = setInterval(function() { sliderTiming(); }, 4000);
+
+  // function to reset time show
   function resetTiming() {
     clearInterval(timingRun);
     timingRun = setInterval(function() { sliderTiming(); }, 4000);
   }
 
   // check title page active
-  function set_Page_Active(element, node) {
+  function setPageActive(element, node) {
     if ($('body').hasClass(element)) {
       $('.item').find(node).addClass('active');
     } else {
       $('.item').find(node).removeClass('active');
     }
   }
-  // get tag select
-  set_Page_Active('tastemaker', '.item__tastemakers');
-  set_Page_Active('blog', '.item__blogs');
-  set_Page_Active('a-list', '.item__vendors');
-  set_Page_Active('contribute', '.item__contribute');
-  set_Page_Active('about', '.item__about');
-
-  $(window).bind('load', function() {
-    $('img').lazyload({
-      effect: 'fadeIn'
-    });
-  });
 
 }( jQuery ));
