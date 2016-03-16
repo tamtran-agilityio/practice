7ccd88504fc472a99ac9a80274ec10333c1cf7e7
Tam Tran <tam.tran@asnet.com.vn>
2016-03-16 15:46:06 +0700

    Show list comment

--- Javascript-Practice/HackerNews/app/scripts/compoment/newest.js
+++ Javascript-Practice/HackerNews/app/scripts/compoment/newest.js
@@ -1,5 +1,9 @@
+/*
+ *
+ */
+
 import { ServerNews } from '../service/get-data';
-import { ModelNew } from '../model/model';
+import { ModelNew } from '../model/model-news';
 export class NewWest {
 	constructor() {
 
@@ -21,15 +25,15 @@ export class NewWest {
 				</span>
 			</td>
 		</tr>
-		<tr>
+		<tr id='${newItem['news'].id}' class = "itemComment">
 			<td class="subtext">
 				<span class="score">${newItem['news'].score} point by
 				</span>
 				<a href="">${newItem['news'].by}</a>
 				<span class="age">
-					<a href="">${time} hour ago </a>
+					<a href="">${time} hours ago </a>
 				</span>
-				<a href="">${newItem['news'].descendants} comment</a>
+				<a href="#" class="itemComment"> ${newItem['news'].descendants} comment </a>
 				<a href=""></a>
 				<a href=""></a>
 			</td>
@@ -37,24 +41,73 @@ export class NewWest {
 		return html;
 	}
 
-	static newHacker(id) {
-		var collectData = new ServerNews();
-			collectData.getItemNews(id).then(function(data) {
-			let dataNews = JSON.parse(data);
+	static renderItemComent(commentItem) {
+		console.log(commentItem);
+		let time = (((commentItem['news'].time)/60)%60).toFixed(0);
+		let htmlComment =
+		`<tr>
+			<td class="ind">
+				<a href="${commentItem['news'].by}"> ${commentItem['news'].by} </a>
+			</td>
+			<td class="votelinks">
+			</td>
+			<td class="default">
+				<div>
+					<span class="comhead">
+						<span class="age"> ${time} hours ago
+						</span>
+					</span>
+				</div>
+				<span class="comment">
+					<span class="c00">${commentItem['news'].text}</span>
+				</span>
+			</td>
+		</tr>`;
+		return htmlComment;
+	}
+
+	static itemNews(id) {
+		let collectData = new ServerNews();
+			collectData.getItemNews(id).then(function(newItem) {
+			let dataNews = JSON.parse(newItem);
 			let modelNews = new ModelNew(dataNews);
 			let item = NewWest.renderItem(modelNews);
-			let $tableBody = $("#view-all-email");
+			let $tableBody = $("#view-all-news");
 			$tableBody.append(item);
 		});
 	}
 
+	static itemComment(id ) {
+		let collectData = new ServerNews();
+			collectData.getItemComment(id).then(function(dataComment) {
+			let dataNews = JSON.parse(dataComment);
+			let modelNews = new ModelNew(dataNews);
+			let itemtComment = NewWest.renderItemComent(modelNews);
+			let $tableBody = $("#view-all-comment");
+			$tableBody.append(itemtComment);
+		});
+	}
+
 	newListHacker() {
-		var collectData = new ServerNews();
-			collectData.getListNews().then(function(data) {
-			let dataNews = JSON.parse(data);
-				dataNews.forEach( function(element) {
-					NewWest.newHacker(element);
-				})
+		let collectData = new ServerNews();
+		collectData.getListNews().then(function(listItem) {
+
+		let listNews = JSON.parse(listItem);
+
+			listNews.forEach( function(element) {
+				NewWest.itemNews(element);
+			})
+		});
+
+		$('#tableNews').delegate('tr.itemComment', 'click', function(event) {
+			$("#view-all-comment").empty();
+			let idNewItem = (this.id).toString();
+			collectData.getItemNews(idNewItem).then(function(data) {
+				let dataNews = JSON.parse(data);
+					(dataNews.kids).forEach(function(element) {
+						NewWest.itemComment(element);
+					})
+			});
 		});
 	}
 }
---
+++ Javascript-Practice/HackerNews/app/scripts/model/model-comments.js
@@ -0,0 +1,5 @@
+export class ModelComment {
+	constructor(comment) {
+		this.comment = comment;
+	}
+}
