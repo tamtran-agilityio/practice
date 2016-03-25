e83fe40f32ddd492272475995e9ee8be12127427
Tam Tran <tam.tran@asnet.com.vn>
2016-03-25 10:50:10 +0700

    Fix show routing example

--- Javascript-Example/Angular-Routing/app/hero-detail.component.ts
+++ Javascript-Example/Angular-Routing/app/hero-detail.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from 'angular2/core';
-import { RouterParams } from 'angular2/router';
+import { RouteParams } from 'angular2/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
-	selecter: 'my-hero-detail',
+	selector: 'my-hero-detail',
 	templateUrl: 'app/hero-detail.component.html',
-  styleUrls: ['app/hero-detail.component.css'],
-  inputs: ['hero']
+	styleUrls: ['app/hero-detail.component.css'],
+	inputs: ['hero']
 })
-
 export class HeroDetailComponent implements OnInit {
 	hero: Hero;
+
 	constructor(
-		private _heroService : HeroService,
-		private _routerParams: RouterParams
-		){ }
-	ngOnInit() {
-		let id = + this._routerParams.get(id);
-		this._heroService.getHero(id).then(hero => this.hero = hero);
+		private _heroService: HeroService,
+		private _routeParams: RouteParams) {
 	}
+
+	ngOnInit() {
+		let id = +this._routeParams.get('id');
+		this._heroService.getHero(id)
+			.then(hero => this.hero = hero);
+	}
+
 	goBack() {
 		window.history.back();
 	}
--- Javascript-Example/Angular-Routing/app/hero.service.ts
+++ Javascript-Example/Angular-Routing/app/hero.service.ts
@@ -4,18 +4,19 @@ import { Injectable } from 'angular2/core';
 
 @Injectable()
 export class HeroService {
-  getHeroes() {
-    return Promise.resolve(HEROES);
-  }
-  getHeroesSlowly() {
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
-    );
-  }
+	getHeroes() {
+		return Promise.resolve(HEROES);
+	}
 
-  getHero(id: number) {
-    return Promise.resolve(HEROES).then(
-      heroes => heroes.filter(hero => hero.id === id)[0]
-    );
-  }
+	getHeroesSlowly() {
+		return new Promise<Hero[]>(resolve =>
+			setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+		);
+	}
+
+	getHero(id: number) {
+		return Promise.resolve(HEROES).then(
+			heroes => heroes.filter(hero => hero.id === id)[0]
+		);
+	}
 }
