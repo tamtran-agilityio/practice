a72aaf3ac682b11640cb3d8c11b4dc25f5b341ca
Tam Tran <tam.tran@asnet.com.vn>
2016-03-31 17:31:32 +0700

    Example about http

---
+++ Javascript-Example/Angular-http/app/main.ts
@@ -0,0 +1,11 @@
+import {bootstrap}         from 'angular2/platform/browser';
+
+import 'rxjs/Rx';
+
+// import {WikiComponent}        from './wiki/wiki.component';
+// import {WikiSmartComponent} from './wiki/wiki-smart.component';
+import {TohComponent}         from './toh/toh.component';
+
+// bootstrap(WikiComponent);
+// bootstrap(WikiSmartComponent);
+bootstrap(TohComponent);
