057a7cf072a5e2b0d2cee30a709bd1ea6708f632
Tam Tran <tam.tran@asnet.com.vn>
2016-04-08 16:21:10 +0700

    update file index

--- Javascript-Practice/Local-Market/src/app/main.ts
+++ Javascript-Practice/Local-Market/src/app/main.ts
@@ -1,4 +1,8 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from './typescript/app.component';
+import { bootstrap } from 'angular2/platform/browser';
+import { AppComponent } from './typescript/app.component';
+import { WhatCooking } from './typescript/what-see/what-see.component';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 
 bootstrap(AppComponent);
--- Javascript-Practice/Local-Market/src/app/typescript/app.component.ts
+++ Javascript-Practice/Local-Market/src/app/typescript/app.component.ts
@@ -1,9 +1,16 @@
-import {Component} from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { HTTP_PROVIDERS }    from 'angular2/http';
+import { WhatCooking } from './what-see/what-see.component';
+import { RecipesService } from './service/service.component';
+import { Recipes } from './recipes/recipes';
+import { RecipesList } from './recipes/recipes-list.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/typescript/app.component.html',
-  styleUrls: ['app/typescript/app.component.css']
+  styleUrls: ['app/typescript/app.component.css'],
+  directives: [ WhatCooking, RecipesList ],
+  providers: [ HTTP_PROVIDERS , RecipesService]
 })
 export class AppComponent {
 	titleNew = 'News';
