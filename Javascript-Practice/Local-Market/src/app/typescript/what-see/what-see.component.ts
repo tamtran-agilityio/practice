b11b0e2dad11a28e9c1cba14ae30bcb319d3f0b9
Tam Tran <tam.tran@asnet.com.vn>
2016-04-08 16:19:13 +0700

    Create what see file

--- Javascript-Practice/Local-Market/src/app/typescript/what-see/what-see.component.ts
+++ Javascript-Practice/Local-Market/src/app/typescript/what-see/what-see.component.ts
@@ -1,12 +1,10 @@
 import { Component } from 'angular2/core';
 
-
 @Component({
 	selector: 'what-see',
 	templateUrl: 'app/typescript/what-see/what-see.component.html',
-	styleUrl: ['app/typescript/what-see/what-see.component.css']
+	styleUrls: ['app/typescript/what-see/what-see.component.css']
 })
 
 export class WhatCooking {
-
 }
