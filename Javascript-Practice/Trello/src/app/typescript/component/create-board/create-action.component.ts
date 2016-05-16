e5b07ec14961d3878e00ac6b2a1ad68a2fb068c8
Tam Tran <tam.tran@asnet.com.vn>
2016-05-16 08:44:54 +0700

    create pop up create action

---
+++ Javascript-Practice/Trello/src/app/typescript/component/create-board/create-action.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'create-action',
+  templateUrl: 'app/typescript/component/create-board/create-action.component.html',
+  styleUrls: ['app/typescript/component/create-board/create-action.component.css']
+})
+
+export class CreatAction {
+
+}
