59efe69a68ed763b538a2df430371eb1fb2ad963
Tam Tran <tam.tran@asnet.com.vn>
2016-07-01 16:02:42 +0700

    Delete all file

--- example-bitters/src/js/main.js
+++
@@ -1,8 +0,0 @@
-/* file javscript */
-$(function() {
-  $('.header-top').click(function () {
-    $('.header-top').slideToggle("slow");
-  });
-  $('.header-top').css({"display":"none","transition":"none","max-height":"inherit"});
-  $('#toggleMenu').remove();
-});
