e62f667a7e59e8cf27371a852dba29f5b87f5244
Tam Tran <tam.tran@asnet.com.vn>
2016-08-04 18:06:25 +0700

    Fix testing component list card

--- React-Practice/React-Trello/test/components/CardComponent.js
+++ React-Practice/React-Trello/test/components/CardComponent.js
@@ -5,6 +5,7 @@ import { expect } from 'chai';
 import ReactAddOn, {addon} from 'react/addons';
 import createComponent from '../helpers/shallowRenderHelper';
 import Card from '../../app/js/components/Card';
+import AddCard from '../../app/js/components/AddCard';
 
 describe('Card component', () => {
   it('should have className \'list-wrapper\'', () => {
@@ -14,5 +15,24 @@ describe('Card component', () => {
     });
 
     expect(CardComponent.props.className).to.contain('list-wrapper');
+  });
+
+  it('should show form add card ', () => {
+    let rootReducers ={
+      card: {
+        showFrom: true
+      }
+    };
+
+    const AddCardComponent = createComponent(AddCard, {
+      state: {
+        rootReducer: rootReducers
+      },
+      onSelectAddCard: () => {},
+      addCardItem: () => {},
+      outSideAddCard: () => {}
+    });
+
+    expect(AddCardComponent.props.className).to.contain('add-list-card');
   });
 });
