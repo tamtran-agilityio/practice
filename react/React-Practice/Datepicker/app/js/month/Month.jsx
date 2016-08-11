247ec204358350bead39718df4443f4e6f73d29b
Tam Tran <tam.tran@asnet.com.vn>
2016-08-11 17:09:33 +0700

    Handel show month current

---
+++ react/React-Practice/Datepicker/app/js/month/Month.jsx
@@ -0,0 +1,51 @@
+import React, {PropTypes, Component} from 'react';
+import ReactDOM from 'react-dom';
+import {clone, toStringDay, toDayOfMonthString, toMonthAndYearString, moveToDayOfWeek, getFullYear} from '../helpers/DateUtilities';
+import WeekContent from '../week/WeekContent';
+
+export default class Month extends Component {
+  constructor(props, def) {
+    super(props);
+  }
+
+  getWeekStartDates(view) {
+    view.setDate(1);
+    view = moveToDayOfWeek(clone(view), 0);
+
+    let current = clone(view);
+    current.setDate(current.getDate()+7);
+
+    let starts = [view],
+    month = current.getMonth();
+    let firstDay = {
+      day: clone(view)
+    }
+    let arr = [];
+    arr.push(firstDay);
+    while (current.getMonth() === month) {
+      let newdate = {
+        day: clone(current)
+      };
+      arr.push(newdate);
+      current.setDate(current.getDate()+7);
+    }
+    console.log("SSSSS AAAAA", arr);
+    return arr;
+  }
+
+  render() {
+    let starts = this.getWeekStartDates(this.props.dayCurrent);
+    return (
+      <div className="month-picker">
+        {
+          starts.map((start, i) => 
+            <WeekContent
+              key= {i}
+              {...start}
+            />
+          )
+        }
+      </div>
+    )
+  }
+}
