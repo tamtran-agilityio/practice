d6075043b760e83c2dae5b26ce61dd53f22805a2
Tam Tran <tam.tran@asnet.com.vn>
2016-08-23 17:11:51 +0700

    fix handle create element on DOM

--- react/Virtual-DOM/app/js/lib/element.js
+++ react/Virtual-DOM/app/js/lib/element.js
@@ -1,116 +1,68 @@
 import * as until from './util' ;
 
-function Element (tagName, props, children) {
-  if (!(this instanceof Element)) {
-    if (!until.isArray(children) && children != null) {
-      children = until.slice(arguments, 2).filter(until.truthy)
-    }
-    return new Element(tagName, props, children)
+class Element {
+  constructor(tagName, props, children) {
+    this.createElementItem(tagName, props, children);
   }
 
-  if (until.isArray(props)) {
-    children = props
-    props = {}
+  /**
+   * @details [create elememt item]
+   * 
+   * @param tagName [tag used]
+   * @param props [style]
+   * @param children [string]
+   */
+  createElementItem(tagName, props, children) {
+    if (!(this instanceof Element)) {
+      if (!until.isArray(this.children) && this.children !== null) {
+        this.children = until.slice(arguments, 2).filter(until.truthy)
+      }
+      return new Element(this.tagName, this.props, this.children)
+    }
+
+    if (until.isArray(props)) {
+      children = props;
+      props = {};
+    }
+
+    this.tagName = tagName;
+    this.props = props || {};
+    this.children = children || [];
+    this.key = props ? props.key : void 666;
+
+    let count = 0;
+
+    until.each(this.children, (child, i) => {
+      if (child instanceof Element) {
+        count += child.count;
+      } else {
+        children[i] = `${child}`;
+      }
+      count++;
+    })
+
+    this.count = count;
   }
 
-  this.tagName = tagName
-  this.props = props || {}
-  this.children = children || []
-  this.key = props
-    ? props.key
-    : void 666
+  /**
+   * @details [render to hmtl]
+   */
+  render() {
+    const element = document.createElement(this.tagName);
+    const props = this.props;
 
-  let count = 0
-
-  until.each(this.children, function (child, i) {
-    if (child instanceof Element) {
-      count += child.count
-    } else {
-      children[i] = '' + child
+    for (const propName in props) {
+      const propValue = props[propName];
+      until.setAttr(element, propName, propValue)
     }
-    count++
-  })
 
-  this.count = count;
+    until.each(this.children, child => {
+      const childEl = (child instanceof Element) ? child.render() : document.createTextNode(child);
+      element.appendChild(childEl);
+    })
+
+    return element
+  }
 }
 
-/**
- * Render the hold element tree.
- */
-Element.prototype.render = function () {
-  let el = document.createElement(this.tagName)
-  let props = this.props
-
-  for (let propName in props) {
-    let propValue = props[propName]
-    until.setAttr(el, propName, propValue)
-  }
-
-  until.each(this.children, function (child) {
-    let childEl = (child instanceof Element)
-      ? child.render()
-      : document.createTextNode(child)
-    el.appendChild(childEl)
-  })
-
-  return el
-}
-
-module.exports = Element
-
-// class Element {
-//   constructor(tagName, props, children) {
-//     if (!(this instanceof Element)) {
-//       if (!until.isArray(children) && children != null) {
-//         children = until.slice(arguments, 2).filter(until.truthy)
-//       }
-//       return new Element(tagName, props, children)
-//     }
-
-//     if (until.isArray(props)) {
-//       children = props
-//       props = {}
-//     }
-
-//     this.tagName = tagName
-//     this.props = props || {}
-//     this.children = children || []
-//     this.key = props
-//       ? props.key
-//       : void 666
-
-//     let count = 0;
-
-//     until.each(this.children, (child, i) => {
-//       if (child instanceof Element) {
-//         count += child.count
-//       } else {
-//         children[i] = `${child}`
-//       }
-//       count++
-//     })
-
-//     this.count = count
-//   }
-
-//   static render() {
-//     const el = document.createElement(this.tagName);
-//     const props = this.props;
-
-//     for (const propName in props) {
-//       const propValue = props[propName];
-//       until.setAttr(el, propName, propValue)
-//     }
-
-//     until.each(this.children, child => {
-//       const childEl = (child instanceof Element)
-//         ? child.render()
-//         : document.createTextNode(child);
-//       el.appendChild(childEl)
-//     })
-
-//     return el
-//   }
-// }
-
-// export default Element;
+export default Element;
