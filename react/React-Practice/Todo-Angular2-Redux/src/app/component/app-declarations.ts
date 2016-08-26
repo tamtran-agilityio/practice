a17e5b7d05bb0056eadac0af69fb9c69ab333ff4
Tam Tran <tam.tran@asnet.com.vn>
2016-08-26 18:09:04 +0700

    hanlde component show filter

---
+++ react/React-Practice/Todo-Angular2-Redux/src/app/component/app-declarations.ts
@@ -0,0 +1,15 @@
+import {AddTodo} from './add-todo';
+import {TodoList} from './list-todo';
+import {Todo} from './todo';
+import {Filters} from './filters';
+import {FilterLink} from './filterLink';
+import {VisibleTodosPipe} from './visibleTodosPipe';
+
+export const APP_DECLARATIONS = [
+  AddTodo,
+  Todo,
+  TodoList,
+  Filters,
+  FilterLink,
+  VisibleTodosPipe
+];
