b690fbfca6a4c6e5d726da7d7157414b86178508
Tam Tran <tam.tran@asnet.com.vn>
2016-08-30 13:50:58 +0700

    handle action clear all complete

--- react/React-Practice/Todo-Angular2-Redux/src/app/reducer/todoReducer.ts
+++ react/React-Practice/Todo-Angular2-Redux/src/app/reducer/todoReducer.ts
@@ -9,7 +9,9 @@ const initialState = {
 }
 
 export function todoReducer(state = initialState, action) {
+
   switch (action.type) {
+
     case 'ADD_TODO':
       return {
         todos: state.todos.concat({
@@ -18,28 +20,58 @@ export function todoReducer(state = initialState, action) {
           completed: action.completed
         }),
         currentFilter: state.currentFilter,
-        count: todoCollection.countTodo()
+        count: countTodo(state.todos, action) + 1
       };
     case 'TOGGLE_TODO':
       return {
         todos: toggleTodo(state.todos, action),
         currentFilter: state.currentFilter,
-        count: todoCollection.countTodo()
+        count: countTodo(state.todos, action)
       };
     case 'REMOVE_TODO':
       return {
-        todos: state.todos.filter(todo => todo.id != action.id),
-        currentFilter: state.currentFilter
+        todos: state.todos.filter(todo => todo.id !== action.id),
+        currentFilter: state.currentFilter,
+        count: countTodo(state.todos, action)
       };
     case 'SET_CURRENT_FILTER':
       return { 
         todos: state.todos.map(todo => todo),
-        currentFilter: action.filter
+        currentFilter: action.filter,
+        count: countTodo(state.todos, action)
       };
+    case 'CLEAR_COMPLETED':
+      return {
+        todos: state.todos.filter(todo => todo.completed !== true),
+        currentFilter: state.currentFilter,
+        count: countTodo(state.todos, action)
+      }
+    case 'COMPLETE_ALL':
+      let countItem = 0;
+      if (action.checked === true) {
+        countItem = 0;
+      } else {
+        countItem = state.todos.length;
+      }
+      return {
+        todos: completeAllTodo(state.todos, action.checked),
+        currentFilter: state.currentFilter,
+        count: countItem
+      }
     default:
       return state;
   }
 };
+
+function countTodo(todos, action) {
+  let countItem = 0;
+  toggleTodo(todos, action).map(todo => {
+    if (todo.completed === false) {
+      countItem ++;
+    }
+  });
+  return countItem;
+}
 
 function toggleTodo(todos, action){
   //map returns new array
@@ -52,6 +84,18 @@ function toggleTodo(todos, action){
       id: todo.id,
       text: todo.text,
       completed: !todo.completed
+    };
+  });
+}
+
+function completeAllTodo(todos, checked) {
+  //map returns new array
+  return todos.map(todo => {
+    //toggle all
+    return {
+      id: todo.id,
+      text: todo.text,
+      completed: checked
     };
   });
 }
