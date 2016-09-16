ca4c2e528ea6ff4ffede44caf837b8378ee41d47
Tam Tran <tam.tran@asnet.com.vn>
2016-09-16 17:53:44 +0700

    create structure practice

--- php_basic/database.php
+++ php_basic/database.php
@@ -3,12 +3,12 @@
   * Mysql database class - only one connection alowed
   */
   class Database {
-    public $_connection;
-    public static $_instance;
-    public $_host = "127.0.0.1";
-    public $_username = "root";
-    public $_password = "root";
-    public $_database = "example";
+    private $connections;
+    private static $_instance;
+    private $_host = "127.0.0.1";
+    private $_username = "root";
+    private $_password = "root";
+    private $_database = "example";
 
     /* Get an instance of the database 
      * return instance
@@ -23,7 +23,8 @@
 
     // Constructor
     private function __construct() {
-      Datatabse::_connection = mysqli_connect($this->_host, $this->_username, $this->_password, $this->_database);
+      echo "string";
+      $this->connections = mysqli_connect($this->_host, $this->_username, $this->_password, $this->_database);
       // Error handling
       if (mysqli_connect_errno()) {
         throw new Exception("MySQL connection error: " . mysqli_connect_error());
@@ -35,9 +36,7 @@
     
     // Get mysqli connection
     public static function getConnection() {
-      echo "string";
-      return Database::_connection;
+      return $this->connections;
     }
   }
-
 ?>
--- php_basic/index.php
+++ php_basic/index.php
@@ -8,41 +8,41 @@
 
   <?php
 
-    include 'database.php';
-    // $foo = new Database;
-   
+    // include 'database.php';
+    // // $foo = new Database;
+
     // Database::getConnection();
-  
+    
     // $mysqli = $db->getConnection(); 
     // $sql_query = "SELECT * FROM  dogs";
     // $result = $mysqli->query($sql_query);
     // var_dump($result);
-    // $link = mysqli_connect("127.0.0.1", "root", "root", "example");
+    $link = mysqli_connect("127.0.0.1", "root", "root", "example");
 
-    // if (mysqli_connect_errno()) {
-    //   throw new Exception("MySQL connection error: " . mysqli_connect_error());
-    // }
+    if (mysqli_connect_errno()) {
+      throw new Exception("MySQL connection error: " . mysqli_connect_error());
+    }
 
-    // echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-    // echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
     /* Select queries return a resultset on tables */
-    // if ($result = $mysqli->query("SELECT * FROM dogs LIMIT 10")) {
-    //   printf("Select returned %d rows.\n", $result->num_rows);
-    //   if ($result->num_rows > 0) {
-    //    // output data of each row
-    //     while($row = $result->fetch_assoc()) {
-    //       echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " Description:" . $row["description"] . "<br>";
-    //     }
-    //   } else {
-    //     echo "0 results";
-    //   }
-    //   /* free result set */
-    //   $result->close();
-    // }
+    if ($result = $link->query("SELECT * FROM dogs LIMIT 10")) {
+      printf("Select returned %d rows.\n", $result->num_rows);
+      if ($result->num_rows > 0) {
+       // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " Description:" . $row["description"] . "<br>";
+        }
+      } else {
+        echo "0 results";
+      }
+      /* free result set */
+      $result->close();
+    }
 
-    // msql_free_result(result);
-    // mysqli_close($mysqli);
+    msql_free_result(result);
+    mysqli_close($mysqli);
   ?>
 
 </body>
