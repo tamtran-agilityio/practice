ed806899a110666423d61455fa956a4e58a3d260
Tam Tran <tam.tran@asnet.com.vn>
2016-09-19 17:27:18 +0700

    change rename folder

--- php_basic/index.php
+++ php_basic/index.php
@@ -1,49 +1,124 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Connect data</title>
-</head>
-<body>
+<?php 
+  define('ROOT', dirname(realpath(__FILE__)).'/');
+  $thisDir = explode('/', ROOT); // 0
+  var_dump($thisDir);
+  $conStrLen = strlen(array_pop($thisDir));
+  $strPos = substr(__FILE__, 0,strrpos( __FILE__, '/')); //javascript-training/php_basic/shopcart"
+  $docRoot = substr($SERVER['DOCUMENT_ROOT'], strrpos($SERVER['DOCUMENT_ROOT'], $SERVER['PHP_SELF'])); // ""
+  $conbineString = substr($strPos, strlen($docRoot));
+  // /javascript-training/php_basic/shopcart
+  $posConf = strlen($conbineString) - $conStrLen; // 39
+  $conLink = substr($conbineString, $posConf); // ""
+  $host = 'http://' . $SERVER['SERVER_NAME'] . '/' .$conLink; // http://
 
-  <?php
+  define('ROOT_URL', $host);
 
-    // include 'database.php';
-    // // $foo = new Database;
+  include(ROOT . 'config/config.php');
+  include(ROOT .'lib/Function.php');
 
-    // Database::getConnection();
-    
-    // $mysqli = $db->getConnection(); 
-    // $sql_query = "SELECT * FROM  dogs";
-    // $result = $mysqli->query($sql_query);
-    // var_dump($result);
-    $link = mysqli_connect("127.0.0.1", "root", "root", "example");
+  /**
+  * Set error reporting
+  */
 
-    if (mysqli_connect_errno()) {
-      throw new Exception("MySQL connection error: " . mysqli_connect_error());
+function setErrorLogging() {
+
+    if ( DEVELOPMENT_ENVIRONMENT == true ) {
+        error_reporting(E_ALL);
+        ini_set('display_errors', "1");
+
+    } else {
+        error_reporting(E_ALL);
+        ini_set('display_errors', "0");
     }
 
-    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+    ini_set('log_errors', "1");
+    ini_set('error_log',ROOT . 'system/log/error_log.php');
+}
 
-    /* Select queries return a resultset on tables */
-    if ($result = $link->query("SELECT * FROM dogs LIMIT 10")) {
-      printf("Select returned %d rows.\n", $result->num_rows);
-      if ($result->num_rows > 0) {
-       // output data of each row
-        while($row = $result->fetch_assoc()) {
-          echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " Description:" . $row["description"] . "<br>";
+
+/** Main Call Function **/
+function callHook() {
+
+    global $url;
+    global $area;
+
+    $url = rtrim($url,"/");
+    $urlArray = array();
+    $urlArray = explode("/",$url);
+    $controller = DEFAULT_CONROLLER;
+    $action = DEFAULT_ACTION;
+
+    //Check if the call is to admin area
+    if ( $urlArray[0] === "admin" ) {
+        $area = "admin";
+        array_shift($urlArray);
+    }
+
+    //Controller
+    if ( isset($urlArray[0]) && !empty($urlArray[0]) ) {
+        $controller = array_shift($urlArray);
+    }
+
+    //Action
+    if ( isset($urlArray[0]) && !empty($urlArray[0]) ) {
+
+        $action = array_shift($urlArray);
+
+        // echo $area . "-->".$controller."--->".$action; exit;
+
+        if ( $controller === "product" && !in_array($action, array('category', 'add', 'addToCart', 'addProcess')) ) {
+            $action = "productDetail";
         }
-      } else {
-        echo "0 results";
-      }
-      /* free result set */
-      $result->close();
+
+        if ( $controller === "category") {
+            $action = "category";
+        }
     }
 
-    msql_free_result(result);
-    mysqli_close($mysqli);
-  ?>
+    //Setup database
+    Database::getInstance('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
 
-</body>
-</html>
+
+    //LOAD THE CONTROLLER
+    $controllerName = $controller;
+    $controller = ucwords($controller);
+    $model = rtrim($controller, 's');
+    $controller .= 'Controller';
+    $dispatch = new $controller();
+
+    // echo $area . "-->".$controller."--->".$action;
+
+    if ( (int)method_exists($controller, $action) ) {
+        call_user_func(array($dispatch,$action));
+
+    } else {
+        error_log("Unknown page/action, Controller = ".$controller.", action = ".$action);
+    }
+}
+
+function __autoload($className) {
+
+    $paths = array(
+        ROOT."/lib/",
+        ROOT."/site/controller/",
+        ROOT."/admin/controller/",
+        ROOT."/common/",
+        ROOT."/common/model/"
+    );
+
+    foreach ( $paths as $path ) {
+
+        if ( file_exists($path.$className.".class.php") ) {
+
+            require_once($path.$className.".class.php");
+            break;
+        }
+    }
+}
+
+$area = "site";
+
+setErrorLogging();
+callHook();
+
+?>
